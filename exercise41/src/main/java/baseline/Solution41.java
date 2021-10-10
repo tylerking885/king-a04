63eca717dc48ee3ec60a5db8aa3dc537cc689f2f
Tyler <tylerking885@knights.ucf.edu>
2021-10-10 10:26:21 -0400

    added pseudocode for exercise41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 41
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Solution41 {
+    public static void main(String[] args) throws Exception {
+        // Create a new object of the main class.
+
+        // Assign the return value of input method to a list string variable.
+        // Pass the List String variable along with output
+        // String file to a writeOutput method.
+    }
+
+    public List<String> readInput(String inputFile) throws IOException {
+        // Create a new FileReader object and pass it String input.txt argument.
+        // Create a new BufferedReader object and pass it the FileReader argument.
+
+        // Declare String variable for input.
+        // Declare new Array list variable and cast it to List String.
+
+        // Loop through input from text file and add it to the list.
+
+        // Close the FileReader input stream.
+
+        // Return the list.
+    }
+
+    public void writeOutput(String outputFile, List<String> lineList) throws IOException {
+        // Sort the list (alphabetically).
+
+        // Construct a FileWriter object and pass it a String output.txt argument.
+        // Construct a PrintWriter object and pass it the fileWriter variable as a parameter.
+
+        // Print the Total # of names according to the size of the list.
+        // Print out a borderline.
+
+        // Loop through the list, printing out its contents.
+
+        // Flush the PrintWriter stream.
+        // Close the PrintWriter stream.
+        // Close the fileWriter stream.
+    }
+}
