46310059eb9fa64a14b4038d773307fb6f2aafc2
Tyler <tylerking885@knights.ucf.edu>
2021-10-10 10:40:28 -0400

    added code to exercise41

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -5,45 +5,76 @@
 
 package baseline;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution41 {
     public static void main(String[] args) throws Exception {
         // Create a new object of the main class.
+        Solution41 app = new Solution41();
 
         // Assign the return value of input method to a list string variable.
+        List<String> lineList =  app.readInput("exercise41_input.txt");
         // Pass the List String variable along with output
         // String file to a writeOutput method.
+        app.writeOutput("exercise41_output.txt", lineList);
     }
 
     public List<String> readInput(String inputFile) throws IOException {
+
         // Create a new FileReader object and pass it String input.txt argument.
+        FileReader fileReader = new FileReader(inputFile);
+
         // Create a new BufferedReader object and pass it the FileReader argument.
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
         // Declare String variable for input.
+        String inputLine;
+
         // Declare new Array list variable and cast it to List String.
+        List<String> lineList = new ArrayList<>();
 
         // Loop through input from text file and add it to the list.
+        while ((inputLine = bufferedReader.readLine()) != null) {
+            lineList.add(inputLine);
+        }
 
         // Close the FileReader input stream.
+        fileReader.close();
 
         // Return the list.
+        return lineList;
     }
 
     public void writeOutput(String outputFile, List<String> lineList) throws IOException {
+
         // Sort the list (alphabetically).
+        Collections.sort(lineList);
 
         // Construct a FileWriter object and pass it a String output.txt argument.
+        FileWriter fileWriter = new FileWriter(outputFile);
+
         // Construct a PrintWriter object and pass it the fileWriter variable as a parameter.
+        PrintWriter out = new PrintWriter(fileWriter);
 
         // Print the Total # of names according to the size of the list.
+        out.printf("Total of %d names%n", lineList.size());
+
         // Print out a borderline.
+        out.println("-----------------");
 
         // Loop through the list, printing out its contents.
+        for (String outputLine : lineList) {
+            out.println(outputLine);
+        }
 
         // Flush the PrintWriter stream.
+        out.flush();
         // Close the PrintWriter stream.
+        out.close();
         // Close the fileWriter stream.
+        fileWriter.close();
     }
 }
