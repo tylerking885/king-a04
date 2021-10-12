efe6884ef43ed48f2ba480788a533aa4a6f9986f
Tyler <tylerking885@knights.ucf.edu>
2021-10-11 21:27:33 -0400

    added test code to exercise45

---
+++ exercise45/src/test/java/baseline/Solution45Test.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+
+class Solution41Test {
+
+    @Test
+    public void readCorrectInputTest() throws IOException {
+        Solution45 test = new Solution45();
+
+        List<String> expected_value = List.of(new String[]{"One should never use the word \"use\" in writing." +
+                " Use \"use\" instead.", "For example, \"She uses an IDE to write " +
+                "her Java programs\" instead of \"She",
+                "uses an IDE to write her Java programs\"."});
+
+        List<String> actual_value = test.readInput("exercise45_input.txt");
+
+        Assertions.assertEquals(expected_value, actual_value, String.valueOf(0));
+    }
+}
