c92622b8a9576a6211ba85e11c44d08ea6b535ae
Tyler <tylerking885@knights.ucf.edu>
2021-10-11 20:19:11 -0400

    Added pseudocode

---
+++ exercise42/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+V
+&src/main/java/baseline/Solution42.java,c\f\cf2d704d5d2f3d0af870581692ca07d745f0c0d9
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,62 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 42
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class Solution42 {
+    public static void main(String[] args) throws Exception {
+        // Construct a new Solution42 object.
+
+        // Assign the return value of readInput method to a List String variable.
+        // Call the writeOutput method by passing in the previous List String
+        // variable and pass in the outputFile name in the form of a String.
+    }
+
+    public List<String> readInput(String inputFile) throws IOException {
+
+        // Construct a fileReader object and pass in the inputFile as
+        // a parameter in the form of a String.
+
+        // Construct a bufferedReader object and pass it the fileReader as a parameter.
+
+        // Declare a new String
+
+        // Construct a new ArrayList object and set it to a ListString variable.
+
+        // Loop through each line of input and assign it to the new string variable.
+
+        // Split the String at each comma.
+        // Assign each part a value.
+
+        // Add the parts to the list.
+
+        // Close the fileReader object stream.
+
+        // return the list.
+    }
+
+    public void writeOutput(String outputFile, List<String> lineList) throws IOException {
+        // Use a counter for each line break.
+
+        // Construct a fileWriter object and pass it the outputFile String as a parameter.
+        // Construct a PrintWriter object and pass it the fileWriter variable as a parameter.
+
+        // Print the Header.
+
+        // Loop through the contents of the list.
+
+        // Indenting the line every 3 entries.
+
+        // Flush the printWriter stream.
+        // Close the printWriter stream.
+        // Close the fileWriter stream.
+    }
+    }
+}
