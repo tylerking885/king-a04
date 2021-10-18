a74f04a9527c0627dffc002521b9ce6fa3387b49
Tyler <tylerking885@knights.ucf.edu>
2021-10-17 20:07:52 -0400

    added pseudocode to exercise43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,61 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution43
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+public class Solution43 {
+
+    public static void main(String[] args) {
+        // Prompt User for Site.
+        // Prompt user for author.
+        // Ask user a question.
+        // Ask user another question.
+        // Set string path variable to root directory.
+        // Add site name to path.
+        // Call createWebsiteFolder method with String myPath as a parameter.
+
+        // If user wants a JS folder
+        // Call method createJavaScriptFolder with String myPath as a parameter.
+
+        // If user wants a CSS folder
+        // Call method createCssFolder with String myPath as a parameter.
+
+        // Close the scanner object.
+    }
+
+    // Method for prompting User for input.
+    private void promptUser(String prompt) {
+        // prompt.
+    }
+
+    public void createJavaScriptFolder(String myPath) {
+
+        // Create a File object variable and set it to new jspath variable.
+        // Create the new directory with the jScriptFolder.
+        // If directory is created print out its creation path.
+    }
+
+    public void createCssFolder(String myPath){
+
+        // Create a File object variable and set it to new csspath variable.
+        // Create the new directory with the CSSFolder.
+        // If directory is created print out its creation path.
+    }
+
+    public void createWebsiteFolder(String myPath){
+        // Create a File object variable and
+        // set it to String Path variable
+
+        // Create the new directory with the website folder.
+
+        // Create a File Writer object to write in html file.
+
+        // The html file contents with the name of the site inside
+        // the <title> tag and the author in a <meta> tag.
+
+        // Close the File object.
+
+    }
+}
