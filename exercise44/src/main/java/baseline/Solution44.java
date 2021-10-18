8f197a4d0fe8f7d258a68a531bd31f149d5f5561
Tyler <tylerking885@knights.ucf.edu>
2021-10-17 20:59:48 -0400

    added pseudocode for exercise44

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,66 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 44
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Solution44 {
+    // Declare String variable.
+    // Declare JSON object variable.
+    // Declare JSON array variable.
+
+    public static void main (String[]args) throws IOException {
+
+        // Set return value from readFile method call to the JsonObject variable.
+
+        // Set return value from getArray method call to the JsonArray variable.
+
+        // Call product search method with JsonArray variable as a parameter.
+    }
+
+    public JsonObject readFile (String inFile) throws FileNotFoundException {
+
+        // Read json file using parser and store it in obj.
+
+        // Create json object to read internal values.
+        return // JSON object.
+    }
+
+    public JsonArray getArray(JsonObject jsonObject){
+
+        // Reading products array from the file.
+        return // JSON array.
+    }
+
+    public void productSearch(JsonArray products) throws IOException {
+
+        // Buffered reader object for user input.
+
+        // Flag drives how long the next loop lasts.
+
+        // Loop until correct input is found.
+
+        // Prompt User for input.
+
+        // Set user input to a string variable.
+
+        // Loop through the products array.
+
+        // Set the element to a json object variable.
+
+        // Grab the name and set it to element variable.
+
+        // Compare name element with user input.
+
+        // If found print corresponding item and it's contents.
+
+        // If flag is true, then value is not found and continue.
+    }
+}
