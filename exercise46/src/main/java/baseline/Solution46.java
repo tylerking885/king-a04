609bd9be9b06e195303aa96e5841e6603ac2f41b
Tyler <tylerking885@knights.ucf.edu>
2021-10-17 20:37:23 -0400

    added pseudocode to exercise46

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,61 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution46
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+import java.io.IOException;
+
+public class Solution46 {
+
+    // Declare string variable.
+
+    public static void main(String[] args) {
+
+        // Start a try catch block for IOException.
+
+        // Set string variable to readFileIntoString methods return value.
+
+        // Update string variable to editString methods return value.
+
+        // Call countWordsFrequency method with
+        // a string fileContent as a parameter.
+    }
+
+    // Read text file into a single string.
+    public String readFileIntoString (String path) throws IOException {
+
+        // Read the txt file from directory.
+
+       return // Match of the start of string.
+    }
+
+    // Take a multi-line string with spaces and reduces it to one line.
+    public String editString (String fileContent){
+
+        // Regular expression to put every word on the same line.
+
+        // Regular expression to eliminate extra spaces.
+
+        return // edited string
+    }
+
+    // Counts the frequency of each word and prints a histogram.
+    public void countWordsFrequency (String str){
+
+        // Initialize a tree map variable.
+
+        // Split the string to find each word.
+
+        // Loop to iterate over the words.
+
+        // Check if the array element is present in the map.
+
+        // If found, increase the count.
+
+        // Otherwise, count remains the same.
+
+        // Loop to iterate over the elements of the map.
+    }
+}
